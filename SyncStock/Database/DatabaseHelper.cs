d192433ce2030c9dd8fd9ef7cac42a3743b6df18
rohAn <christianrohanvecencio@gmail.com>
2026-05-10 11:05:42 +0800

    Partial backend and DB on PurchaseOrder

---
+++ SyncStock/Database/DatabaseHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyncStock.Database
+{
+    public class DatabaseHelper
+    {
+        private static readonly string _connectionString = "Server=localhost\\SQLEXPRESS;Database=SyncStock;Trusted_Connection=True;";
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
---
+++ SyncStock/Models/PurchaseOrderItem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyncStock.Models
+{
+    public class PurchaseOrderItem
+    {
+        public int PurchaseOrderItemID { get; set; }
+        public int PurchaseOrderID { get; set; }
+        public int ItemID { get; set; }
+        public string ItemName { get; set; }
+
+        private int _quantity;
+        public int Quantity
+        {
+            get => _quantity;
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Quantity must be greater than 0.");
+                _quantity = value;
+            }
+        }
+
+        private decimal _unitPrice;
+        public decimal UnitPrice
+        {
+            get => _unitPrice;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Unit price cannot be negative.");
+                _unitPrice = value;
+            }
+        }
+
+        public decimal TotalPrice => Quantity * UnitPrice;
+    }
+}
