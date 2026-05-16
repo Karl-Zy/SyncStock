90887e8aeab2777d1842d28a092d30961e032c77
Dyncees <cardaluu6@gmail.com>
2026-05-16 07:11:36 -0700

    Added models for report

---
+++ SyncStock/Models/Models[Report]/DashBoardKPIs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyncStock.Models.Models_Report_
+{
+    public class DashBoardKPIs
+    {
+        public decimal TotalCost { get; set; }
+        public int TotalItems { get; set; }
+        public int TotalCategory { get; set; }
+    }
+}
