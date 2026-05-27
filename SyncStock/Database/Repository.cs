5e8a90d325792632906af7f61bf59b8690213a07
Karl Zy <karl.chiong1@gmail.com>
2026-05-27 19:02:20 +0800

    added missing (OPO)

--- SyncStock/Database/Repository.cs
+++ SyncStock/Database/Repository.cs
@@ -503,6 +503,31 @@ namespace SyncStock.Database
                                              AND ci.ItemName = i.ItemName
             ORDER BY po.OrderDate DESC, i.ItemName"
                 ).ToList();
+
+
+
+            }
+
+
+        }
+        public IEnumerable<PurchaseOrderItem> GetAllOPOPurchaseOrderItems()
+        {
+            using (var conn = CreateConnection())
+            {
+                return conn.Query<PurchaseOrderItem>(@"
+                    SELECT 
+                        poi.PurchaseOrderItemID,
+                        poi.PurchaseOrderID,
+                        poi.ItemID,
+                        i.ItemName,
+                        poi.Quantity,
+                        poi.UnitPrice,
+                        (poi.Quantity * poi.UnitPrice) AS TotalPrice
+                    FROM PurchaseOrderItems poi
+                    INNER JOIN Items i ON poi.ItemID = i.ItemID
+                    INNER JOIN PurchaseOrders po ON poi.PurchaseOrderID = po.PurchaseOrderID
+                    WHERE po.POType = 'OPO'
+                    ORDER BY poi.PurchaseOrderItemID DESC");
             }
         }
     }
